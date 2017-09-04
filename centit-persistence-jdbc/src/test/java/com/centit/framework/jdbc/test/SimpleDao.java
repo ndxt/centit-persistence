972fa6bebce0f90cb59e700e2107d6be504034f1
杨淮生 <codefan@centit.com>
2017-09-04 09:33:41 +0800

    完善BaseDaoImpl已和Hibernate保持一致，添加了列名转换

--- centit-persistence-jdbc/src/test/java/com/centit/framework/jdbc/test/SimpleDao.java
+++ centit-persistence-jdbc/src/test/java/com/centit/framework/jdbc/test/SimpleDao.java
@@ -2,12 +2,14 @@ package com.centit.framework.jdbc.test;
 
 import com.centit.framework.jdbc.dao.BaseDaoImpl;
 
+import java.util.Map;
+
 /**
  * Created by codefan on 17-8-31.
  */
 public class SimpleDao extends BaseDaoImpl<SimplePo, String> {
     @Override
-    public String getDaoEmbeddedFilter() {
+    public Map<String, String> getFilterField() {
         return null;
     }
 }
