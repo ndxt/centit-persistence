fbf41a77ffc873771472c266322a6d2eb52ebe56
codefan <codefan@sina.com>
2022-09-06 11:36:44 +0800

    添加密码加密

--- centit-persistence-config/src/main/java/com/centit/framework/core/config/DataSourceConfig.java
+++ centit-persistence-config/src/main/java/com/centit/framework/core/config/DataSourceConfig.java
@@ -8,6 +8,7 @@ import com.centit.support.database.utils.DBType;
 import com.centit.support.database.utils.QueryLogUtils;
 import com.centit.support.security.AESSecurityUtils;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.lang3.StringUtils;
 import org.dom4j.DocumentException;
 import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
@@ -33,44 +34,26 @@ public class DataSourceConfig implements EnvironmentAware {
         }
     }
 
+    public static String encryptProperty(String propertyValue) {
+        if (StringUtils.isNotBlank(propertyValue)) {
+            if (propertyValue.startsWith("cipher:")) {
+                return AESSecurityUtils.decryptBase64String(
+                    propertyValue.substring(7), AESSecurityUtils.AES_DEFAULT_KEY);
+            }
+        }
+        return propertyValue;
+    }
+
     @Bean(destroyMethod = "close")
     public BasicDataSource dataSource() {
-        /*String dataSourcePoolType = env.getProperty("connection.pool.type");
-        if("proxool".equals(dataSourcePoolType)) {
-            ProxoolDataSource dataSource = new ProxoolDataSource();
-            dataSource.setDriver(env.getProperty("jdbc.driver"));
-            dataSource.setDriverUrl(env.getProperty("jdbc.url"));
-            dataSource.setUser(env.getProperty("jdbc.user"));
-            dataSource.setPassword(env.getProperty("jdbc.password"));
-            dataSource.setMaximumConnectionCount(env.getProperty("jdbc.maxActive",Integer.class));
-            dataSource.setTestBeforeUse(true);
-            dataSource.setHouseKeepingSleepTime(600000);//间隔10分钟检查所有连接是否需要关闭或创建
-            dataSource.setMaximumActiveTime(600000);//连接超时时间
-            return dataSource;
 
-        }else if("c3p0".equals(dataSourcePoolType)) {
-            ComboPooledDataSource dataSource = new ComboPooledDataSource();
-            dataSource.setDriverClass(env.getProperty("jdbc.driver"));
-            dataSource.setJdbcUrl(env.getProperty("jdbc.url"));
-            dataSource.setUser(env.getProperty("jdbc.user"));
-            dataSource.setPassword(env.getProperty("jdbc.password"));
-            dataSource.setInitialPoolSize(env.getProperty("jdbc.maxActive", Integer.class));
-            dataSource.setMaxIdleTimeExcessConnections(env.getProperty("jdbc.maxIdle", Integer.class));
-            dataSource.setCheckoutTimeout(env.getProperty("jdbc.maxWait", Integer.class));
-            dataSource.setAcquireIncrement(Integer.parseInt(env.getProperty("jdbc.acquireIncrement")));
-            dataSource.setAcquireRetryAttempts(Integer.parseInt(env.getProperty("jdbc.acquireRetryAttempts")));
-            dataSource.setAcquireRetryDelay(Integer.parseInt(env.getProperty("jdbc.acquireRetryDelay")));
-            dataSource.setIdleConnectionTestPeriod(Integer.parseInt(env.getProperty("jdbc.idleConnectionTestPeriod")));
-            dataSource.setPreferredTestQuery(env.getProperty("jdbc.validationQuery"));
-            return dataSource;
-        }*/
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName(env.getProperty("jdbc.driver"));
         dataSource.setUrl(env.getProperty("jdbc.url"));
         dataSource.setUsername(
-            AESSecurityUtils.decryptParameterString(env.getProperty("jdbc.user")));
+            AESSecurityUtils.decryptParameterString(encryptProperty(env.getProperty("jdbc.user"))));
         dataSource.setPassword(
-            AESSecurityUtils.decryptParameterString(env.getProperty("jdbc.password")));
+            AESSecurityUtils.decryptParameterString(encryptProperty(env.getProperty("jdbc.password"))));
         dataSource.setMaxTotal(NumberBaseOpt.castObjectToInteger(env.getProperty("jdbc.maxActive"),100));
         dataSource.setMaxIdle(NumberBaseOpt.castObjectToInteger(env.getProperty("jdbc.maxIdle"),50));
         dataSource.setMaxWaitMillis(NumberBaseOpt.castObjectToInteger(env.getProperty("jdbc.maxWait"), 2000));
