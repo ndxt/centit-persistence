62b1041d86684b158c976f5c6c7b893744c3b33a
codefan <codefan@centit.com>
2023-02-09 09:45:05 +0800

    修改bug增强代码

--- centit-persistence-extend/src/main/java/com/centit/framework/core/service/DataScopePowerManager.java
+++ centit-persistence-extend/src/main/java/com/centit/framework/core/service/DataScopePowerManager.java
@@ -17,6 +17,7 @@ public interface DataScopePowerManager {
 
     /**
      * 获得用户摸个功能方法的数据范围权限，返回null或者size==0表示拥有所有权限
+     * @param topUnit 用户租户代码
      * @param sUserCode sUserCode
      * @param sOptid sOptid
      * @param sOptMethod sOptMethod
@@ -28,6 +29,7 @@ public interface DataScopePowerManager {
     /**
      * 创建用户数据范围过滤器，和上面的方法结合使用
      * @param userInfo JSONObject 用户信息
+     * @param topUnit 用户租户代码
      * @param currentUnit 用户当前机构
      * @return DataPowerFilter
      */
