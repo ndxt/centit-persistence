57c3db5654a878bea9a79e3476409350fa2621bd
codefan <codefan@centit.com>
2023-02-20 17:58:01 +0800

    修改 字典转换默认值

--- centit-persistence-extend/src/main/java/com/centit/framework/core/dao/PageQueryResult.java
+++ centit-persistence-extend/src/main/java/com/centit/framework/core/dao/PageQueryResult.java
@@ -57,7 +57,7 @@ public class PageQueryResult<T> implements ToResponseData, Serializable {
      */
     public static <D> PageQueryResult<D>
         createResult(Collection<D> objList, PageDesc pageDesc, String[] filterFields){
-        return innerCreateResult(objList, pageDesc, false, filterFields);
+        return innerCreateResult(objList, pageDesc, true, filterFields);
     }
 
     /**
@@ -82,7 +82,7 @@ public class PageQueryResult<T> implements ToResponseData, Serializable {
      */
     public static <D> PageQueryResult<D>
         createResult(Collection<D> objList, PageDesc pageDesc){
-        return innerCreateResult(objList, pageDesc, false, null);
+        return innerCreateResult(objList, pageDesc, true, null);
     }
 
     /**
