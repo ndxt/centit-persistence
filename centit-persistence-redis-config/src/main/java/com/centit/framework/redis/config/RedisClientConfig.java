d8ce3a63ced5a851337ae8a1bcaa1c84ed42594a
codefan <codefan@centit.com>
2024-06-13 09:15:41 +0800

    添加 RedisTemplate 配置文件

--- centit-persistence-redis-config/src/main/java/com/centit/framework/redis/config/RedisClientConfig.java
+++ centit-persistence-redis-config/src/main/java/com/centit/framework/redis/config/RedisClientConfig.java
@@ -1,35 +1,40 @@
 package com.centit.framework.redis.config;
 
+import com.centit.support.algorithm.NumberBaseOpt;
 import com.centit.support.security.SecurityOptUtils;
 import io.lettuce.core.RedisClient;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.Resource;
 
 /**
  * Created by codefan on 2017/6/14.
  */
-@Configuration
-public class RedisClientConfig {
+public class RedisClientConfig implements EnvironmentAware {
+
     private Logger logger = LoggerFactory.getLogger(RedisClientConfig.class);
 
-    @Value("${redis.default.host:}")
-    private String host;
+    protected Environment env;
 
-    @Value("${redis.default.port:6379}")
-    private Integer port;
-
-    @Value("${redis.default.password:}")
-    private String password;
-
-    @Value("${redis.default.database:0}")
-    private Integer database;
+    @Resource
+    @Override
+    public void setEnvironment(Environment environment) {
+        if (environment != null) {
+            this.env = environment;
+        }
+    }
 
     @Bean
     public RedisClient redisClient() {
+        String host = env.getProperty("redis.default.host");
+        Integer port = NumberBaseOpt.castObjectToInteger(env.getProperty("redis.default.port"), 6379);
+        String password = env.getProperty("redis.default.password");
+        Integer database =  NumberBaseOpt.castObjectToInteger(env.getProperty("redis.default.database"), 0);
         // redis:[password@]host[:port][/database]
         StringBuilder redisUri = new StringBuilder("redis://");
         if (StringUtils.isNotBlank(password)) {
