ac912efd8a853453a598e9882175c2ea3c615b6e
codefan <codefan@centit.com>
2025-04-02 15:41:08 +0800

    将bit类型统一位 int ，避免不支持的数据库 单独处理

--- centit-database/src/main/java/com/centit/support/database/utils/FieldType.java
+++ centit-database/src/main/java/com/centit/support/database/utils/FieldType.java
@@ -676,7 +676,8 @@ public abstract class FieldType {
             "UINT64".equalsIgnoreCase(columnType) ||
             "SERIAL".equalsIgnoreCase(columnType)) {
             return FieldType.LONG;
-        } else if ("INT".equalsIgnoreCase(columnType) ||
+        } else if ("BIT".equalsIgnoreCase(columnType) || // SQL Server 有这个类型
+            "INT".equalsIgnoreCase(columnType) ||
             "INT4".equalsIgnoreCase(columnType) ||
             "INT8".equalsIgnoreCase(columnType) ||
             "INT16".equalsIgnoreCase(columnType) ||
